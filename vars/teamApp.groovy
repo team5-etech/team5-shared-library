5b25ef5c59319efc1005b89c68b2f1bd039958e0
Francis <elfrancteetwo@gmail.com>
2023-04-10 21:07:47 -0400

    new groovy file added

--- vars/teamApp.groovy
+++ vars/teamApp.groovy
@@ -1,1 +1,32 @@
-#
+def etech(datatype datavariable){
+pipeline {
+       agent any
+       tools {
+           maven 'maven'
+       }
+       stages {
+           stage("Tools initialization") {
+               steps {
+                   sh 'mvn --version'
+                   sh 'java -version'
+               }
+           }
+           stage("Checkout Code") {
+               steps {
+                   git branch: 'main',
+                          url: "${repoUrl}"
+               }
+           }
+           stage('team5-test-shared'){
+            steps{
+                echo "let's test this"
+            }
+           }
+           stage("to-test-maven") {
+               steps {
+                   sh 'mvn -v'
+               }
+           }
+       }
+   } 
+}
